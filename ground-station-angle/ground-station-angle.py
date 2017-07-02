4f3695cc931fb286804394a2129b93e630a90bbe
ignormies <bryce.beagle@gmail.com>
2017-07-02 09:33:19 -0700

    Initial revision of a script to find times at which a satellite passes over a point on the Earth

---
+++ ground-station-angle/ground-station-angle.py
@@ -0,0 +1,36 @@
+import spiceypy as spice
+import math
+
+spice.furnsh("ISS.bsp")
+spice.furnsh("ASUGS.bsp")
+spice.furnsh("naif0011.tls")
+spice.furnsh("pck00010.tpc")
+
+# we are going to get positions between these two dates
+utc = [spice.str2et('Jun 11, 2017'), spice.str2et('Jun 13, 2017')]
+
+TIMFMT = "YYYY MON DD HR:MN:SC.###### TDB::RND::TDB"
+
+target   = "-125544";
+obsrvr   = "-999999"
+inframe  = "IAU_EARTH"
+crdsys   = "LATITUDINAL"
+coord    = "LATITUDE"
+relate   = ">"
+refval   = math.radians(10)
+step     = 30
+nintvals = 100000
+adjust   = 0.0
+abcorr   = "NONE"
+cnfine   = spice.stypes.SPICEDOUBLE_CELL(200000)
+result   = spice.stypes.SPICEDOUBLE_CELL(200000)
+
+spice.wninsd(*utc, cnfine)
+
+spice.gfposc(target, inframe, abcorr, obsrvr, crdsys, coord, relate, refval, adjust, step, nintvals, cnfine, result)
+
+print(result)
+
+for i in range(spice.wncard(result)):
+    k = spice.wnfetd(result, i)
+    print("{}    {}".format(spice.timout(k[0], TIMFMT), spice.timout(k[1], TIMFMT)))
