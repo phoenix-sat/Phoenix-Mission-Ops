ddc7348a551dd458f254b49796120016be06b428
NickAltman112 <nickaltman96@gmail.com>
2017-08-02 21:30:49 -0700

    Add files via upload

--- Modem/Modem.py
+++ Modem/Modem.py
@@ -21,43 +21,21 @@ class Modem(object):
 	#init sets up a link with the tnc and reads the dictionary and sets the settings to their approprate values
 	def __init__(self):
 		cfg = self.parse_config()
-		#Links to the TNC
-##################################################
-		self.ser = serial.Serial(
-			port=cfg['Serial']['SERIAL_Port'],
-			baudrate=cfg['Serial']['ABAUD'],
-			parity=serial.PARITY_NONE,
-			stopbits=serial.STOPBITS_ONE,
-			bytesize=serial.EIGHTBITS
-			)
+		self.ser = self.Setup_Modem()
+		#connects to the KPC 9612 Plus
 		self.ser.close()
 		self.ser.open()
-#################################################
-
 		#Sets the settings to the desired values
-#################################################
-		count = len(cfg['Terminal'])
+		count = len(cfg['Startup']['Terminal'])
 		out = ''
 		while count > 0:
-			self.ser.write(cfg['Terminal'][count - 1])
+			self.ser.write(cfg['Startup']['Terminal'][count - 1])
 			self.ser.write('\r')
-			count = count - 1
-#################################################		
-
-		#Displays the current settings
-#################################################
-		self.ser.write('DISPLAY' + '\r')
-		time.sleep(5)
-		while self.ser.inWaiting() > 0:
-			out += self.ser.read(1)
-		if out != '':
-			print(">>" + out)
-#################################################
-		return self.ser
+			count = count - 1		
 
 	#adding this in just in case we want to start fresh when activiating the TNC
 	'''def Reboot(self):
-		self.ser = self.__init__()
+		self.ser = self.Setup_Modem()
 		cfg = self.parse_config()
 		self.ser.write(cfg['Calabration']['RESTORE DEFAULT'])
 		time.sleep(8)
@@ -70,15 +48,63 @@ class Modem(object):
 	#I do not have to worry about changeing the HBAUD here because that can be changed in the config
 	#All this will really do is switch from port 1 to port 2 to listen and transmit	
 	def Switch_Port(self):
-		self.ser = self.__init__()
+		out = ''
 		cfg = self.parse_config()
-		User = input("To select port, type 1 or 2")
+		User = input("To select port, type 1 or 2: ")
 		if User == 2:
 			self.ser.write(cfg['PORT']['PORT_2'])
+			self.ser.write('\r')
+			#self.ser.flushInput()
+			'''time.sleep(1)
+			while self.ser.inWaiting() > 0:
+				out += self.ser.read(1)'''
 		if User == 1:
 			self.ser.write(cfg['PORT']['PORT_1'])
+			self.ser.write('\r')
+			#self.ser.flushInput()
+			'''time.sleep(1)
+			while self.ser.inWaiting() > 0:
+				out += self.ser.read(1)'''
+		'''self.ser.write('PORT' + '\r')
+		out = '''''
+		time.sleep(1)
+		while self.ser.inWaiting() > 5:
+			#print(self.ser.inWaiting())
+			out += self.ser.read(1)
+		if out != '':
+			print(out)
+
+
+	#sets up the connection with the TNC without making a link
+	def Setup_Modem(self):
+		cfg = self.parse_config()
+		self.ser = serial.Serial(
+			port=cfg['Serial']['SERIAL_Port'],
+			baudrate=cfg['Serial']['ABAUD'],
+			parity=serial.PARITY_NONE,
+			stopbits=serial.STOPBITS_ONE,
+			bytesize=serial.EIGHTBITS
+			)
+		return self.ser
+
+	#Displays the current settings of the TNC
+	def get_Modem_State(self):
+		out = ''
+		self.ser = self.Setup_Modem()
+		self.ser.write('DISPLAY' + '\r')
+		time.sleep(5)
+		while self.ser.inWaiting() > 5:
+			out += self.ser.read(1)
+		if out != '':
+			print("\nThe current settings:\n" + out)
+
+	def Check_Consistency(self):
+		pass
+
 if __name__ == "__main__":
 	mymodem=Modem()
+	mymodem.Switch_Port()
+	mymodem.get_Modem_State()
 	#print(mymodem.cfg)
 	#mymodem.__init__()
 
--- Modem/Test_Modem/Test_Modem.py
+++ Modem/Test_Modem/Test_Modem.py
@@ -7,12 +7,12 @@ import yaml
 #pty does not work on windows
 import pty
 import os
-
+import mock
 
 class test_Modem(object):
 	
 	def setUp(self):
-		pass
+		self.User_Port = 1 
 		
 		
 	def test_parse_config(self):
@@ -25,11 +25,7 @@ class test_Modem(object):
 
 	def test__init__(self):
 		cfg = self.test_parse_config()
-		#Virtual serial port
-###############################################
-		master, slave = pty.openpty()
-		s_name = os.ttyname(master)
-		self.ser = serial.Serial(s_name)
+		self.ser = self.test_Setup_Modem()
 		self.ser.close()
 		self.ser.open()
 ###############################################
@@ -43,21 +39,10 @@ class test_Modem(object):
 			self.ser.write('\r')
 			count = count - 1
 			time.sleep(1)
-###############################################		
-
-		#Displays the test settings
 ###############################################
-		self.ser.write('DISPLAY' + '\r')
-		time.sleep(5)
-		while self.ser.inWaiting() > 0:
-			out += self.ser.read(1)
-		if out != '':
-			print(">>" + out)
-###############################################
-		return self.ser
 	
 	def test_Reboot(self):
-		self.ser = self.test__init__()
+		self.ser = self.test_Setup_Modem()
 		cfg = self.test_parse_config()
 		self.ser.write(cfg['Calabration']['RESTORE DEFAULT'])
 		time.sleep(8)
@@ -68,14 +53,57 @@ class test_Modem(object):
 
 
 	def test_Switch_Port(self):
-		self.ser = self.test__init__()
+		out = ''
+		self.ser = self.test_Setup_Modem()
 		cfg = self.test_parse_config()
-		User = input("To select port, type 1 or 2")
+		User = self.User_Port
 		if User == 2:
 			self.ser.write(cfg['PORT']['PORT_2'])
+			self.ser.write('\r')
+			#self.ser.flush()
+			'''time.sleep(1)
+			while self.ser.inWaiting() > 0:
+				out += self.ser.read(1)'''
 		if User == 1:
 			self.ser.write(cfg['PORT']['PORT_1'])
+			self.ser.write('\r')
+			#self.ser.flush()
+			'''time.sleep(1)
+			while self.ser.inWaiting() > 0:
+				out += self.ser.read(1)'''
+		'''self.ser.write('PORT' + '\r')
+		out = '''''
+		time.sleep(1)
+		while self.ser.inWaiting() > 5:
+			print(self.ser.inWaiting())
+			out += self.ser.read(1)
+		if out != '':
+			print(out)
 
+	def test_get_Modem_State(self):
+		out = ''
+		self.ser = self.test_Setup_Modem()
+		self.ser.write('DISPLAY' + '\r')
+		time.sleep(5)
+		while self.ser.inWaiting() > 5:
+			out += self.ser.read(1)
+		if out != '':
+			print(out)
+
+
+	def test_Setup_Modem(self):
+		cfg = self.test_parse_config()
+		master, slave = pty.openpty()
+		s_name = os.ttyname(master)
+		self.ser = serial.Serial(s_name)
+		'''self.ser = serial.Serial(
+			port=cfg['Serial']['SERIAL_Port'],
+			baudrate=cfg['Serial']['ABAUD'],
+			parity=serial.PARITY_NONE,
+			stopbits=serial.STOPBITS_ONE,
+			bytesize=serial.EIGHTBITS
+			)'''
+		return self.ser
 		
 	
 
